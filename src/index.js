b01a6820ce59bb2268b22bb523012246ea8dc214
Raheem McDonald <raheemmcdonald@Raheems-MacBook-Pro.local>
2019-06-09 00:53:18 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'animate.css';
 
 ReactDOM.render(
   <Router>
--- src/views/MainNavigation.js
+++ src/views/MainNavigation.js
@@ -7,6 +7,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Badge from 'react-bootstrap/Badge';
 
 // Router
 import { withRouter, Link } from 'react-router-dom';
@@ -15,14 +16,40 @@ const MainNavigation = props => {
   return (
     <React.Fragment>
       {/* Navbar */}
-      <Navbar bg='dark' variant='dark' expand='lg'>
+      <Navbar bg='light' variant='light' expand='lg'>
         <Navbar.Toggle aria-controls='main-navbar-nav' />
 
         <Navbar.Collapse id='main-navbar-nav'>
           <Nav className='ml-auto' as='ul'>
-            <Nav.Link href='/vocab/'>Vocab</Nav.Link>
-            <Nav.Link href='/tenses/'>Tenses</Nav.Link>
-            <NavDropdown title='Tenses' id='basic-nav-dropdown'>
+            <Nav.Link href='/vocab/'>Vocabulary</Nav.Link>
+            <NavDropdown title='Tenses' id='tenses-nav-dropdown'>
+              <NavDropdown.Item href='/tenses/present-tense/'>
+                Present Tense
+              </NavDropdown.Item>
+              <NavDropdown.Item href='/tenses/present-progressive/'>
+                Present Progressive
+              </NavDropdown.Item>
+              <NavDropdown.Item href='/tenses/preterite/'>
+                Preterite Tense
+              </NavDropdown.Item>
+              <NavDropdown.Item href='/tenses/imperfect/'>
+                Imperfect Tense
+              </NavDropdown.Item>
+              <NavDropdown.Item href='/tenses/conditional/'>
+                Conditional Tense
+              </NavDropdown.Item>
+              <NavDropdown.Item href='/tenses/future/'>
+                Future Tense
+              </NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href='/tenses/subjunctive/'>
+                Subjunctive Mood{' '}
+                <Badge pill variant='primary'>
+                  ess
+                </Badge>
+              </NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown title='Orals' id='orals-nav-dropdown'>
               <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
               <NavDropdown.Item href='#action/3.2'>
                 Another action
@@ -33,25 +60,7 @@ const MainNavigation = props => {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href='/orals/'>Orals</Nav.Link>
-            <NavDropdown title='Orals' id='basic-nav-dropdown'>
-              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.2'>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action/3.4'>
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-            <NavItem title='Tenses' to='/tenses/' />
-            <Nav.Link href='//' />
-            <NavItem title='' to='/' />
-            <NavItem title='' to='/' />
-            <NavItem title='' to='/' />
-            <NavItem title='Orals' to='/orals/' />
-            <NavItem title='Conact' to='/' />
+            <Nav.Link href='/practice-question/'>Practice Questions</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
