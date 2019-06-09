3641c5e2ca81f8f805ffad3011cae1184a019f9b
Raheem McDonald <raheemmcdonald@Raheems-MacBook-Pro.local>
2019-06-09 08:28:46 -0500

    Updated <footer />

--- src/views/Footer.js
+++ src/views/Footer.js
@@ -13,19 +13,19 @@ const Footer = props => {
       <Navbar bg='dark' variant='dark'>
         <Nav as='ul' style={centerNavStyle}>
           <Nav.Item as='li'>
+            <Nav.Link href='/privacy-policy/'>Privacy Policy</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as='li'>
+            <Nav.Link href='/terms-of-use/'>Terms Of Use</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as='li'>
+            <Nav.Link href='/contribute/'>Contribute</Nav.Link>
+          </Nav.Item>
+          <Nav.Item as='li'>
             <Nav.Link href='/tutoring/'>Tutoring</Nav.Link>
           </Nav.Item>
           <Nav.Item as='li'>
-            <Nav.Link href='/'>Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href='/'>Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href='/'>Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as='li'>
-            <Nav.Link href='/'>Home</Nav.Link>
+            <Nav.Link href='/contact/'>Contact</Nav.Link>
           </Nav.Item>
         </Nav>
       </Navbar>
